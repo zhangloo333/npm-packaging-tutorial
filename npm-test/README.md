9d12ecf26f556db883be758652292d0d7749cfb6
Lei Zhang(APA) <leizhang@linkedin.com>
2019-06-27 11:25:46 -0700

    1. build a npm packaging

---
+++ npm-test/README.md
@@ -0,0 +1,7 @@
+### Title
+
+This is for npm packing testing purpose.
+
+### Purpose
+
+This will delete later
---
+++ npm-test/index.js
@@ -0,0 +1,3 @@
+module.exports.area = radius => Math.PI * radius;
+module.exports.perimeter = radius => 2 * Math.PI * radius;
+module.exports.words = words => `${words} created by ${new Date()}`;
